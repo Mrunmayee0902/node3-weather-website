ebf5f39e5164ead5e587b04f6cd6678e05fc3cee
Mrunmayee Aundhekar <52483213+Mrunmayee0902@users.noreply.github.com>
2020-06-29 20:41:21 +0530

    Remove unnecessary console.log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,13 +1,3 @@
-console.log('Client side javascript file is loaded')
-// fetch('http://puzzle.mead.io/puzzle').then((response)=>{
-//     response.json().then((data)=>{
-//         console.log(data)
-//     })
-// })
-
-// //fetch is a browser based API
-
-
 const weatherForm=document.querySelector('form')
 const search= document.querySelector('input')
 const messageOne=document.querySelector('#message-1')
@@ -36,29 +26,3 @@ fetch('http://localhost:3000/weather?address='+location).then((response)=>{
 })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Template engine is used to render dynamic web pages using express
-//In this,handlebars template engine is used to render dynamic documents and reuse the created code(for styling)
-//This javascript file will be seen in console in developer options
-//here express converts the hbs view to html
-//partials is the part of code which can be reused in other files
-//For setting up my 404 error page use * (wild card character) :match anything that hasn't been matched so far//views are rendered
